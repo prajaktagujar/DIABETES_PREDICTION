0b71e7c5860d9bca80e1094b6b3d3fe7c04f30bc
PRAJAKTA GUJAR <162875027+prajaktagujar@users.noreply.github.com>
2025-03-15 21:49:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,4 +61,8 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+import os
+
+port = int(os.environ.get("PORT", 5000))  # Get Railway-assigned port
+app.run(host="0.0.0.0", port=port, debug=True)
+
